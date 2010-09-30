2654cabeaaf5d7595ca354e0165945cb25e83029
andrews medina <andrewsmedina@gmail.com>
2010-09-30 02:11:41 -0300

    add inital action class

--- magic.js
+++ magic.js
@@ -40,6 +40,10 @@ var Sprite = new Class({
     initialize: function(image) {
         this.image = new Image();
         this.image.src = image;
+        this.actions = [];
+        this.toRemove = [];
+        this._count = 0;
+        this.scheduled = false;
     },
     position: function(x, y) {
         this.x = x;
@@ -47,6 +51,126 @@ var Sprite = new Class({
     },
     draw: function() {
         director.context.drawImage(this.image, this.x, this.y);
+    },
+    count: function() {
+        this._count++;
+        return this._count;
+    },
+    do: function( action ) {
+       action.setTarget( this );
+      
+       action.start();
+       this.actions.push( action );
+       
+       if ( !this.scheduled ) {
+           this.scheduled = true;
+           
+           var that = this;
+
+           this._interval = setInterval(function(){
+               that._step.call( that, that.count() );
+           }, 1);
+          
+       }       
+    },
+    _step: function(dt) {
+        
+        for each (action in this.toRemove) {
+            if (action in this.actions) {
+                this.actions.remove( action );
+            }
+        }
+        this.toRemove = [];
+        
+        if ( this.actions.length === 0 ) {
+            this.scheduled = false;
+            clearInterval( this._step );
+        }
+
+        this.actions.each(function(action, index) {
+            if ( !action.scheduledToRemove ) {
+                //console.log(action, dt)
+                //debugger;
+                //action.step(dt);
+                if ( action.done() ) {
+                    this.removeAction( action );
+                }
+            }
+            
+        })
+    },
+    removeAction: function(action) {
+        if ( action.scheduledToRemove ) {
+            action.scheduledToRemove = true;
+            action.stop();
+            action.target = null;
+            this.toRemove.push( action );
+        }
+    }
+});
+/*
+/*def _step(self, dt):
+
+    if len( self.actions ) == 0:
+        self.scheduled = False
+        pyglet.clock.unschedule( self._step )
+
+    for action in self.actions:
+        if not action.scheduled_to_remove:
+            action.step(dt)
+            if action.done():
+                self.remove_action( action )
+*/
+
+var Action = new Class({
+    initialize: function() {
+        this.duration = null;
+        this.target = null;
+        this.elapsed = 0;
+        this._done = false;
+        this.scheduledToRemove = false;
+    },
+    start: function() {
+        
+    },
+    stop: function() {
+        this.target = null;
+    },
+    step: function(dt) {
+        this.elapsed += dt;
+    },
+    done: function() {
+        return this._done;
+    }
+});
+
+var IntervalAction = new Class({
+    Extends: Action,
+    step: function(dt) {
+        this.elapsed += dt;
+        this.update( this.elapsed/this.duration );
+    },
+    done: function() {
+        return this.elapsed >= self.duration;
+    }
+});
+
+var ScaleBy = new Class({
+    Extends: IntervalAction,
+    initialize: function(scale, duration) {
+        this.endScale = scale;
+        this.duration = duration;
+    },
+    setTarget: function(sprite) {
+        this.sprite = sprite;
+    },
+    start: function() {
+        this.startScale = this.sprite.scale;
+        this.delta = this.startScale * this.endScale - this.startScale
+    },
+    update: function(t) {
+        //console.log('oiee')
+        this.target.scale = this.startScale + self.delta * t
     }
 });
 
